6e4ba74ecce37d410a3139fb0b7904b8aba60d58
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2022-02-05 13:00:09 -0300

    indentation 4 to 2

--- digz.js
+++ digz.js
@@ -17,54 +17,54 @@ delete argv.givenPortOnly;
 
 const options = {};
 for(const key of Object.keys(argv)) {
-    const value = argv[key];
-    if(
-        key === '_'
-        || key.charAt(0) === '$'
-    )
-        continue;
-    options[key] = value;
+  const value = argv[key];
+  if(
+    key === '_'
+    || key.charAt(0) === '$'
+  )
+    continue;
+  options[key] = value;
 }
 
 if (argv._.length >= 1) {
-    const target = argv._[0];
-    const split = target.split(':');
-    options.host = split[0];
-    if (split.length >= 2) {
-        options.port = split[1];
-    }
+  const target = argv._[0];
+  const split = target.split(':');
+  options.host = split[0];
+  if (split.length >= 2) {
+    options.port = split[1];
+  }
 }
 if (debug) {
-    options.debug = true;
+  options.debug = true;
 }
 if (givenPortOnly) {
-    options.givenPortOnly = true;
+  options.givenPortOnly = true;
 }
 
 const digz = new Digz(options);
 digz.query(options)
-    .then((state) => {
-        if(pretty) {
-            console.log(JSON.stringify(state,null,'  '));
-        } else {
-            console.log(JSON.stringify(state));
-        }
-    })
-    .catch((error) => {
-        if (debug) {
-            if (error instanceof Error) {
-                console.log(error.stack);
-            } else {
-                console.log(error);
-            }
-        } else {
-            if (error instanceof Error) {
-                error = error.message;
-            }
-            if (pretty) {
-                console.log(JSON.stringify({error: error}, null, '  '));
-            } else {
-                console.log(JSON.stringify({error: error}));
-            }
-        }
-    });
+  .then((state) => {
+    if(pretty) {
+      console.log(JSON.stringify(state,null,'  '));
+    } else {
+      console.log(JSON.stringify(state));
+    }
+  })
+  .catch((error) => {
+    if (debug) {
+      if (error instanceof Error) {
+        console.log(error.stack);
+      } else {
+        console.log(error);
+      }
+    } else {
+      if (error instanceof Error) {
+        error = error.message;
+      }
+      if (pretty) {
+        console.log(JSON.stringify({error: error}, null, '  '));
+      } else {
+        console.log(JSON.stringify({error: error}));
+      }
+    }
+  });
